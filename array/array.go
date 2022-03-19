e0d41d344c48f6777b73ec35ce6c3a5708bd454c
Richie Varghese <ric-v@outlook.com>
2022-03-20 00:21:40 +0530

    changed data type for arr

--- array/array.go
+++ array/array.go
@@ -1,17 +1,17 @@
 package array
 
-// dataTypes is a list of all the types that can be used in arrays.
-type dataTypes interface {
+// arrTypes is a list of all the types that can be used in arrays.
+type arrTypes interface {
 	~bool | ~uint | ~uint16 | ~uint32 | ~uint64 | ~int8 | ~int | ~int16 | ~int64 | ~float32 | ~float64 | ~complex64 | ~complex128 | ~uintptr | ~string | ~rune | ~byte
 }
 
 // In returns the index of the first element in the array that equals the searchKey.
-func In[T dataTypes](arr []T, key T) (exists bool, i int, val T) {
+func In[T arrTypes](arr []T, key T) (exists bool, i int, val T) {
 	return in(arr, key)
 }
 
 // private func for In
-func in[T dataTypes](arr []T, key T) (exists bool, i int, val T) {
+func in[T arrTypes](arr []T, key T) (exists bool, i int, val T) {
 	for i, val = range arr {
 		if val == key {
 			return true, i, val
@@ -21,14 +21,14 @@ func in[T dataTypes](arr []T, key T) (exists bool, i int, val T) {
 }
 
 // Replace replaces the first element in the array that equals the searchKey with the newValue.
-func Replace[T dataTypes](arr []T, key T, replacer T) []T {
+func Replace[T arrTypes](arr []T, key T, replacer T) []T {
 	_, i, _ := in(arr, key)
 	arr[i] = replacer
 	return arr
 }
 
 // Map applies the function f to each element of the array and returns the result.
-func Map[T dataTypes](arr []T, f func(T) T) []T {
+func Map[T arrTypes](arr []T, f func(T) T) []T {
 	for i, v := range arr {
 		arr[i] = f(v)
 	}
@@ -36,7 +36,7 @@ func Map[T dataTypes](arr []T, f func(T) T) []T {
 }
 
 // Reduce applies the function f to each element of the array and returns the result.
-func Reduce[T dataTypes](a []T, f func(T, T) T) T {
+func Reduce[T arrTypes](a []T, f func(T, T) T) T {
 	for i := 1; i < len(a); i++ {
 		a[i] = f(a[i-1], a[i])
 	}
@@ -44,7 +44,7 @@ func Reduce[T dataTypes](a []T, f func(T, T) T) T {
 }
 
 // Filter returns a new array containing only the elements that satisfy the predicate f.
-func Filter[T dataTypes](a []T, f func(T) bool) []T {
+func Filter[T arrTypes](a []T, f func(T) bool) []T {
 	var b []T
 	for _, v := range a {
 		if f(v) {
@@ -55,7 +55,7 @@ func Filter[T dataTypes](a []T, f func(T) bool) []T {
 }
 
 // RemoveAt removes the first element in the array that equals the searchKey.
-func RemoveAt[T dataTypes](a []T, searchKey T) []T {
+func RemoveAt[T arrTypes](a []T, searchKey T) []T {
 	_, i, _ := in(a, searchKey)
 	// check if i is out of bounds
 	if i+1 > len(a) {
@@ -65,7 +65,7 @@ func RemoveAt[T dataTypes](a []T, searchKey T) []T {
 }
 
 // InsertAt inserts the newValue at the index i.
-func InsertAt[T dataTypes](a []T, i int, v T) []T {
+func InsertAt[T arrTypes](a []T, i int, v T) []T {
 	// check if i is out of bounds
 	if i > len(a) {
 		return append(a, v)
@@ -74,7 +74,7 @@ func InsertAt[T dataTypes](a []T, i int, v T) []T {
 }
 
 // AppendAt appends the newValue at the index i.
-func AppendAt[T dataTypes](a []T, i int, v []T) []T {
+func AppendAt[T arrTypes](a []T, i int, v []T) []T {
 	// check if i is out of bounds
 	if i > len(a) {
 		return append(a, v...)
